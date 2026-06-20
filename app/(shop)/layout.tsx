5b56cf7bddb0fee38cf0d872b494143ca2d6ae8e
Javier Gimenez <jsgimenez@gmail.com>
2026-06-19 21:07:59 -0300

    feat: adds top menu

--- app/(shop)/layout.tsx
+++ app/(shop)/layout.tsx
@@ -1,7 +1,9 @@
+import { TopMenu } from "@/components";
 
 export default function ShopLayout({ children }: { children: React.ReactNode; }) {
     return (
-        <main className="min-h-screen bg-red-500">
+        <main className="min-h-screen">
+            <TopMenu />
             {children}
         </main>
     );
---
+++ components/ui/top-menu/topMenu.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link"
+import { IoCartOutline, IoSearchOutline } from "react-icons/io5"
+
+import { titleFont } from "@/config/fonts"
+
+export const TopMenu = () => {
+    return (
+        <nav className="flex px-5 justify-between items-center w-full">
+            <div>
+                <Link href="/">
+                    <span className={`${titleFont.className} antialiased font-bold`}>
+                        pataflafla
+                    </span>
+                    <span className={`${titleFont.className} antialiased`}>
+                        .io
+                    </span>
+
+                </Link>
+            </div>
+            <div className="hidden sm:block">
+                <Link className="m-2 p-2 rounded-md transition-all hover:bg-gray-100" href="/category/men">hombres</Link>
+                <Link className="m-2 p-2 rounded-md transition-all hover:bg-gray-100" href="/category/women">mujeres</Link>
+                <Link className="m-2 p-2 rounded-md transition-all hover:bg-gray-100" href="/category/kids">niños</Link>
+            </div>
+            <div className="flex items-center">
+                <Link href="/search" className="mx-2">
+                    <IoSearchOutline className="w-5 h-5" />
+                </Link>
+                <Link href="/search" className="mx-2">
+                    <div className="relative">
+                        <span className="absolute -top-2 -right-2 px-1 text-xs rounded-full bg-emerald-600 font-bold text-white">3</span>
+                        <IoCartOutline className="w-5 h-5" />
+                    </div>
+                </Link>
+                <button className="m-2 p-2 rounded-md transition-all hover:bg-gray-100">menu</button>
+            </div>
+        </nav>
+    )
+}
