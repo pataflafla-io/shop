749c63ca6947d2bbae644d97428123e6deee2d3b
Javier Gimenez <jsgimenez@gmail.com>
2026-06-21 18:32:41 -0300

    styles: add notFound customized for product

---
+++ app/(shop)/product/not-found.tsx
@@ -0,0 +1,7 @@
+import { PageNotFound } from "@/components/ui";
+
+export default function () {
+    return (
+        <PageNotFound message="No existe ese producto en nuestro catálogo." />
+    );
+}
