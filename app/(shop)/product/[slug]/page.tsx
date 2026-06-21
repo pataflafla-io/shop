6fa80c45c722e71a60e85ece7f298e241e63444f
Javier Gimenez <jsgimenez@gmail.com>
2026-06-21 18:34:42 -0300

    feat: add swiper dep for slideshow

--- app/(shop)/product/[slug]/page.tsx
+++ app/(shop)/product/[slug]/page.tsx
@@ -1,8 +1,38 @@
+import { notFound } from "next/navigation";
+import { titleFont } from "@/config/fonts";
+import { QuantitySelector, SizeSelector } from "@/components";
 
-export default function () {
+import { initialData } from "@/seed/seed";
+
+interface Props {
+    params: {
+        slug: string
+    }
+}
+
+export default async function ({ params }: Props) {
+    const { slug } = await params;
+    const product = initialData.products.find(product => product.slug === slug)
+
+    if (!product) {
+        notFound()
+    }
+
     return (
-        <div>
-            <h1>Product Page</h1>
+        <div className="mt-5 mb-20 grid md:grid-cols-3 gap-3">
+
+            <div className="col-span-1 md:col-span-2">
+                <h1>Hola mundo</h1>
+            </div>
+            <div className="col-span-1 px-5">
+                <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>{product.title}</h1>
+                <p className="text-lg mb-5">${product.price}</p>
+                <SizeSelector selectedSize={product.sizes[0]} availableSizes={product.sizes} />
+                <QuantitySelector quantity={1} />
+                <button className="btn-primary my-5">agregar al carrito</button>
+                <h3 className="font-black text-sm">Descripción</h3>
+                <p className="font-light">{product.description}</p>
+            </div>
         </div>
     );
 }
