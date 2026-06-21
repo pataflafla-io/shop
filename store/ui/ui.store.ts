83c50898b52c3f5b17d58556a89d73ee8ec12f83
Javier Gimenez <jsgimenez@gmail.com>
2026-06-20 21:29:38 -0300

    feat: adds zustand ui store

---
+++ store/ui/ui.store.ts
@@ -0,0 +1,13 @@
+import { create } from 'zustand'
+
+interface State {
+    isSidebarOpen: boolean;
+    openSidebar: () => void;
+    closeSidebar: () => void;
+}
+
+export const useUIStore = create<State>()((set) => ({
+    isSidebarOpen: false,
+    openSidebar: () => { set({ isSidebarOpen: true }) },
+    closeSidebar: () => { set({ isSidebarOpen: false }) }
+}))
