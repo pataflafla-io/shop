4149aa3143a2699892e276c481c772cf716fcd5d
Javier Gimenez <jsgimenez@gmail.com>
2026-06-21 18:31:55 -0300

    styles: updates  barrel file

--- components/index.ts
+++ components/index.ts
@@ -1,2 +1,4 @@
+export * from './product/quantity-selector/QuantitySelector'
+export * from './product/size-selector/SizeSelector'
 export * from './products/grid/ProductsGrid'
 export * from './products/grid/ProductsGridItem'
