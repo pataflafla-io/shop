65d5c7e5abd8631f2aef323d712560600f83ce7f
Javier Gimenez <jsgimenez@gmail.com>
2026-06-21 16:51:33 -0300

    feat: category page with 404 error support

--- app/(shop)/category/[id]/page.tsx
+++ app/(shop)/category/[id]/page.tsx
@@ -1,8 +1,38 @@
+import { notFound } from "next/navigation";
+import { ProductsGrid } from '@/components';
+import { Title } from '@/components/ui';
 
-export default function NamePage() {
+import { initialData } from '@/seed/seed';
+import { Category } from "@/interfaces";
+import { isKeyObject } from "util/types";
+const products = initialData.products
+
+interface Props {
+    params: {
+        id: Category
+    }
+}
+export default async function ({ params }: Props) {
+    const { id } = await params;
+
+    const categoryType = ["men", "women", "kid", "unisex"]
+    if (!categoryType.includes(id)) {
+        notFound();
+    }
+
+    const label: Record<Category, string> = {
+        "men": "para hombres",
+        "women": "para mujeres",
+        "kid": "para niños",
+        "unisex": "unisex"
+    }
+
+    const productsByCategory = products.filter(product => product.gender === id)
+
     return (
-        <div>
-            <h1>Hello Page</h1>
-        </div>
+        <>
+            <Title title={`Artículos ${label[id]}`} subtitle="Todos los productos." />
+            <ProductsGrid products={productsByCategory} />
+        </>
     );
 }
