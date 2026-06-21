94301999564ad0b38143ca34d0e95a854ffcea49
Javier Gimenez <jsgimenez@gmail.com>
2026-06-20 21:30:58 -0300

    feat: implements open/close sidebar using zustand state

--- components/ui/sidebar/Sidebar.tsx
+++ components/ui/sidebar/Sidebar.tsx
@@ -3,13 +3,24 @@
 import Link from "next/link"
 import { IoCloseOutline, IoLogInOutline, IoLogOutOutline, IoPeopleOutline, IoPersonOutline, IoSearch, IoSearchOutline, IoShirtOutline, IoTicketOutline } from "react-icons/io5"
 
+import { useUIStore } from "@/store";
+import clsx from "clsx";
+
 export const Sidebar = () => {
+    const isSidebarOpen = useUIStore(state => state.isSidebarOpen);
+    const closeSidebar = useUIStore(state => state.closeSidebar);
     return (
         <div>
-            <div className="fixed z-10 top-0 left-0 w-full h-screen bg-black opacity-50" />
-            <div className="fade-in fixed z-10 top-0 left-0 w-full h-screen backdrop-filter backdrop-blur-md" />
-            <nav className="fixed z-20 top-0 right-0 p-5 w-100 h-screen bg-white shadow-2xl transition-all duration-300">
-                <IoCloseOutline size={50} className="absolute top-5 right-5" onClick={() => console.log('close')} />
+            {isSidebarOpen && (
+                <div className="fixed z-10 top-0 left-0 w-full h-screen bg-black opacity-50" />
+            )}
+            {isSidebarOpen && (
+                <div onClick={closeSidebar} className="fade-in fixed z-10 top-0 left-0 w-full h-screen backdrop-filter backdrop-blur-md" />
+            )}
+
+
+            <nav className={clsx("fixed z-20 top-0 right-0 p-5 w-100 h-screen bg-white shadow-2xl transition-all duration-300", { "translate-x-full": !isSidebarOpen })}>
+                <IoCloseOutline size={50} className="absolute top-5 right-5" onClick={closeSidebar} />
                 <div className="relative mt-14">
                     <IoSearchOutline size={20} className="absolute top-2 left-2" />
                     <input type="text" placeholder="search" className="w-full bg-gray-50 rounded px-10 py-1 border-b-2 text-xl border-gray-200 focus:outline-none focus:border-emerald-500" />
