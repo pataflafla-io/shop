5185cb1d8f20e168056f57e6028f432900f3aad8
Javier Gimenez <jsgimenez@gmail.com>
2026-06-22 23:17:10 -0300

    styles: updates styles and markup

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,8 +1,10 @@
 
 export default function ShopLayout({ children }: { children: React.ReactNode; }) {
     return (
-        <main className="min-h-screen bg-blue-500">
-            {children}
+        <main className="flex justify-center">
+            <div className="w-full sm:w-87.5 px-5">
+                {children}
+            </div>
         </main>
     );
 }
