352300a12aa1fbb3da69e4c4556ffce8cae1c4d7
Javier Gimenez <jsgimenez@gmail.com>
2026-06-22 23:19:07 -0300

    styles: updates barrel files

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,6 @@
 export * from './product/quantity-selector/QuantitySelector'
 export * from './product/size-selector/SizeSelector'
+export * from './product/slideshow/ProductMobileSlideshow'
+export * from './product/slideshow/ProductSlideshow'
 export * from './products/grid/ProductsGrid'
 export * from './products/grid/ProductsGridItem'
--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,3 +1,4 @@
+export * from './footer/Footer'
 export * from './not-found/PageNotFound'
 export * from './sidebar/Sidebar'
 export * from './title/Title'
