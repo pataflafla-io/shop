2718dd8d1937747580c2813ca35c1156b3061bb1
Javier Gimenez <jsgimenez@gmail.com>
2026-06-27 22:23:56 -0300

    feat(gender): implement error page for enabling custom messages

---
+++ app/(shop)/gender/error.tsx
@@ -0,0 +1,17 @@
+'use client'
+import { PageNotFound } from "@/components/ui";
+
+
+interface Props {
+    error: Error
+}
+
+// Este componente se usa como 404 cuando se genera una excepción
+// El parámetro recibido tiene el mensaje de error enviado desde
+// el servidor
+export default function ({ error }: Props) {
+    const { message } = error;
+    return (
+        <PageNotFound message={message} />
+    );
+}
