dad8c0674fb00a37202872a31da4ec7e189fd5ae
Javier Gimenez <jsgimenez@gmail.com>
2026-06-27 23:04:37 -0300

    chore(products): add cache and pagination

--- app/(shop)/page.tsx
+++ app/(shop)/page.tsx
@@ -1,15 +1,33 @@
+// Mantiene en cache por 1 minuto
+export const revalidate = 60;
+
+import { redirect } from 'next/navigation';
+
 import { ProductsGrid } from '@/components';
-import { Title } from '@/components/ui';
+import { Pagination, Title } from '@/components/ui';
+import { getPaginatedProductsWithImages } from '../actions';
 
-import { initialData } from '@/seed/seed';
-const products = initialData.products
+interface Props {
+  searchParams: {
+    page?: string,
+  }
+}
 
-export default function Home() {
+export default async function Home({ searchParams }: Props) {
+  const { page } = await searchParams
+  const pageNumber = page ? parseInt(page) : 1
+  const { products, totalPages } = await getPaginatedProductsWithImages({ page: pageNumber })
+
+  if (products.length === 0) redirect("/")
+
   return (
 
     <>
-      <Title title="Tienda" subtitle="Todos los productos" />
+      <Title title="Store" subtitle="All products" />
       <ProductsGrid products={products} />
+      {totalPages > 0 &&
+        <Pagination totalPages={totalPages} />
+      }
     </>
 
   );
