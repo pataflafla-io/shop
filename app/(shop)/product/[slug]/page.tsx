c6de9a5019f5c79b9d59ee5cb04ee0b38645be74
Javier Gimenez <jsgimenez@gmail.com>
2026-06-27 23:07:51 -0300

    chore(seo): add dynamic metadata. refactor add to cart

--- app/(shop)/product/[slug]/page.tsx
+++ app/(shop)/product/[slug]/page.tsx
@@ -1,8 +1,11 @@
-import { notFound } from "next/navigation";
-import { titleFont } from "@/config/fonts";
-import { ProductMobileSlideShow, ProductSlideShow, QuantitySelector, SizeSelector } from "@/components";
+// En cache por 7 días
+export const revalidate = 10080;
 
-import { initialData } from "@/seed/seed";
+import { Metadata, ResolvingMetadata } from "next";
+import { titleFont } from "@/config/fonts";
+import { ProductMobileSlideShow, ProductSlideShow, StockLabel } from "@/components";
+import { getProductBySlug } from "@/app/actions";
+import { AddToCart } from "./ui/addToCart";
 
 interface Props {
     params: {
@@ -10,17 +13,37 @@ interface Props {
     }
 }
 
-export default async function ({ params }: Props) {
-    const { slug } = await params;
-    const product = initialData.products.find(product => product.slug === slug)
+// Función para genera Metadata dinámica basada en el slug
+// que llega por url.
+// https://nextjs.org/docs/app/getting-started/metadata-and-og-images
 
-    if (!product) {
-        notFound()
+export async function generateMetadata(
+    { params }: Props,
+    parent: ResolvingMetadata
+): Promise<Metadata> {
+
+    const slug = (await params).slug
+
+    const product = await getProductBySlug(slug)
+
+    return {
+        title: product.title,
+        description: product.description || '',
+        openGraph: {
+            title: product.title,
+            description: product.description || '',
+            // Url absoluta
+            images: [`/product/${product.images[0]}`]
+        }
     }
+}
+
+export default async function ProductPage({ params }: Props) {
+    const { slug } = await params;
+    const product = await getProductBySlug(slug)
 
     return (
         <div className="mt-5 mb-20 grid md:grid-cols-3 gap-3">
-
             <div className="col-span-1 md:col-span-2">
                 <ProductMobileSlideShow className="block sm:hidden" title={product.title} images={product.images} />
                 <ProductSlideShow className="hidden sm:block" title={product.title} images={product.images} />
@@ -28,10 +51,8 @@ export default async function ({ params }: Props) {
             <div className="col-span-1 px-5">
                 <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>{product.title}</h1>
                 <p className="text-lg mb-5">${product.price}</p>
-                <SizeSelector selectedSize={product.sizes[0]} availableSizes={product.sizes} />
-                <QuantitySelector quantity={1} />
-                <button className="btn-primary my-5">agregar al carrito</button>
-                <h3 className="font-black text-sm">Descripción</h3>
+                <AddToCart product={product} />
+                <h3 className="font-black text-sm">Description</h3>
                 <p className="font-light">{product.description}</p>
             </div>
         </div>
