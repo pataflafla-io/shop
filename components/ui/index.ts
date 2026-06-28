83fd35abacf7026f744a745551f35b5d16f61c7e
Javier Gimenez <jsgimenez@gmail.com>
2026-06-27 22:15:24 -0300

    chore(exports): update barrel file to export new components

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,5 +1,6 @@
 export * from './footer/Footer'
 export * from './not-found/PageNotFound'
+export * from './pagination/Pagination'
 export * from './sidebar/Sidebar'
 export * from './title/Title'
 export * from './top-menu/TopMenu'
