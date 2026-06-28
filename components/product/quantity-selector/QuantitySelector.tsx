00903b9c08e3dd1e4b94bb3666034d7fa0c9b9a3
Javier Gimenez <jsgimenez@gmail.com>
2026-06-27 22:54:06 -0300

    feat(components): add maxQuantity and onQuantityChange to QuantitySelector component

--- components/product/quantity-selector/QuantitySelector.tsx
+++ components/product/quantity-selector/QuantitySelector.tsx
@@ -1,27 +1,27 @@
 'use client'
 
-import { useState } from "react";
 import { IoAddCircleOutline, IoRemoveCircleOutline } from "react-icons/io5";
 
 interface Props {
+    maxQuantity?: number;
     quantity: number;
+    onQuantityChange: (quantity: number) => void;
 }
 
-export const QuantitySelector = ({ quantity }: Props) => {
-    const [count, setCount] = useState(quantity)
-    const onQuantityChange = (value: number) => {
-        if (count + value < 1) {
+export const QuantitySelector = ({ maxQuantity, quantity, onQuantityChange }: Props) => {
+    const onValueChange = (value: number) => {
+        if (quantity + value < 1 || (maxQuantity && quantity + value > maxQuantity)) {
             return
         }
-        setCount(count + value)
+        onQuantityChange(quantity + value)
     }
     return (
         <div className="flex">
-            <button onClick={() => onQuantityChange(-1)}>
+            <button onClick={() => onValueChange(-1)}>
                 <IoRemoveCircleOutline size={20} />
             </button>
-            <span className="w-20 mx-3 px-5 bg-gray-100 text-center rounded">{count}</span>
-            <button onClick={() => onQuantityChange(+1)}>
+            <span className="w-20 mx-3 px-5 py-2 bg-gray-200 text-center rounded">{quantity}</span>
+            <button onClick={() => onValueChange(+1)}>
                 <IoAddCircleOutline size={20} />
             </button>
         </div>
