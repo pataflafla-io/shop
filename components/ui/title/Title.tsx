56940dec2958063faa1e883b6122a4579fc05d40
Javier Gimenez <jsgimenez@gmail.com>
2026-06-27 22:57:50 -0300

    feat(components): implement title component with subtitle support

--- components/ui/title/Title.tsx
+++ components/ui/title/Title.tsx
@@ -9,7 +9,7 @@ interface Props {
 export const Title = ({ title, subtitle, className }: Props) => {
     return (
         <div className={`mt-3 ${className}`}>
-            <h1 className={`${titleFont.className} antialiased text-4xl font-semibold`}>
+            <h1 className={`${titleFont.className} antialiased text-3xl font-semibold`}>
                 {title}
             </h1>
             {subtitle && <h2 className="text-xl mb-10">{subtitle}</h2>}
