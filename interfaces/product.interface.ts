397db3cbd18cf6e9addc6a5b5ee577e2de8ce41d
Javier Gimenez <jsgimenez@gmail.com>
2026-06-27 21:53:59 -0300

    feat(product): update Product interface for cart

--- interfaces/product.interface.ts
+++ interfaces/product.interface.ts
@@ -1,5 +1,5 @@
 export interface Product {
-    //@todo: id: string;
+    id: string;
     description: string;
     images: string[];
     inStock: number;
@@ -8,10 +8,21 @@ export interface Product {
     slug: string;
     tags: string[];
     title: string;
-    type: Type;
-    gender: Category;
+    //
+    // type: Type;
+    gender: Gender;
 }
 
-export type Category = 'men' | 'women' | 'kid' | 'unisex'
+export interface CartProduct {
+    id: string;
+    image: string;
+    price: number;
+    quantity: number;
+    size: Size;
+    slug: string;
+    title: string;
+}
+
+export type Gender = 'men' | 'women' | 'kids' | 'unisex'
 export type Size = 'XS' | 'S' | 'M' | 'L' | 'XL' | 'XXL' | 'XXXL';
 export type Type = 'shirts' | 'pants' | 'hoodies' | 'hats';
