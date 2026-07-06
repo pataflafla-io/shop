31eb4f2d74bd61c996932d9eae331b4d3ad27260
Javier Gimenez <jsgimenez@gmail.com>
2026-07-06 17:48:16 -0300

    feat(auth): add proxy middleware

---
+++ proxy.ts
@@ -0,0 +1,11 @@
+import NextAuth from 'next-auth'
+import { authConfig } from './auth.config'
+
+export default NextAuth(authConfig).auth;
+
+export const config = {
+    matcher: [
+        // Exclude API routes, static files, image optimizations, and .png files
+        '/((?!api|_next/static|_next/image|.*\\.png$).*)',
+    ],
+}
