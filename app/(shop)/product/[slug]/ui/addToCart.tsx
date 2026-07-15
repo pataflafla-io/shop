94e14f8140137413eb689d2674528118556596b6
Javier Gimenez <jsgimenez@gmail.com>
2026-07-14 23:36:24 -0300

    refactor: implements useGender

--- app/(shop)/product/[slug]/ui/addToCart.tsx
+++ app/(shop)/product/[slug]/ui/addToCart.tsx
@@ -1,7 +1,11 @@
 'use client';
 
-import { useEffect, useState } from "react"
-
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { getStockBySlug } from '@/app/actions/products/getStockBySlug';
+import { QuantitySelector, SizeSelector } from '@/components';
+import { titleFont } from '@/config/fonts';
+import { CartProduct, Product, Size } from '@/interfaces';
 // La página ProductPage se mantiene en cache por 7 días
 // Este componente se usa para que esta sección no se mantenga
 // en cache si cambia en algún momento.
@@ -9,82 +13,103 @@ import { useEffect, useState } from "react"
 // de ProductPage (price, sizes, etc)
 
 import { useCartStore } from '@/store';
-import { getStockBySlug } from "@/app/actions/products/getStockBySlug"
-import { CartProduct, Product, Size } from '@/interfaces';
-import { titleFont } from "@/config/fonts"
-import { QuantitySelector, SizeSelector } from '@/components'
-import { setCookie } from "cookies-next";
+import { useGenderSection } from '@/store/ui/genderSection.store';
+import { setCookie } from 'cookies-next';
+import { toast } from 'sonner';
+import { json } from 'zod';
 
 interface Props {
-    product: Product
+  product: Product;
 }
 
 export const AddToCart = ({ product }: Props) => {
+  const [stock, setStock] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
 
-    setCookie("gender", product.gender)
-    const [stock, setStock] = useState(0)
-    const [isLoading, setIsLoading] = useState(true)
+  const getStock = async () => {
+    setIsLoading(true);
+    const stock = await getStockBySlug(product.slug);
+    setStock(stock);
+    setIsLoading(false);
+  };
 
-    const getStock = async () => {
-        setIsLoading(true)
-        const stock = await getStockBySlug(product.slug)
-        setStock(stock);
-        setIsLoading(false)
+  const setStore = useGenderSection((state) => state.setCurrentGender);
+  useEffect(() => setStore(product.gender), []);
+
+  useEffect(() => {
+    getStock();
+  }, []);
+
+  const addProductToCart = useCartStore((state) => state.addProductToCart);
+
+  const [selectedSize, setSelectedSize] = useState<Size | undefined>();
+  const [selectedQuantity, setSelectedQuantity] = useState<number>(1);
+  const [addedToCart, setAddedToCart] = useState<boolean>(false);
+
+  const productToCart: CartProduct = {
+    id: product.id,
+    image: product.images[0],
+    price: product.price,
+    quantity: selectedQuantity,
+    size: selectedSize!,
+    slug: product.slug,
+    title: product.title,
+  };
+
+  const addToCart = () => {
+    setAddedToCart(true);
+    if (!selectedSize) {
+      return;
     }
 
-    useEffect(() => {
-        getStock()
-    }, [])
+    addProductToCart(productToCart);
 
-    const addProductToCart = useCartStore(state => state.addProductToCart)
+    // Una vez agregado al carrito,
+    // reseteamos las elecciones
+    setAddedToCart(false);
+    setSelectedQuantity(1);
+    setSelectedSize(undefined);
+    toast.success(
+      <div>
+        {product.title} was added to the <Link href="/cart">cart</Link>
+      </div>
+    );
+  };
 
-    const [selectedSize, setSelectedSize] = useState<Size | undefined>();
-    const [selectedQuantity, setSelectedQuantity] = useState<number>(1);
-    const [addedToCart, setAddedToCart] = useState<boolean>(false);
+  return (
+    <>
+      {isLoading ? (
+        <h3
+          className={`${titleFont.className} p-1 bg-gray-100 text-xl rounded antialiased animate-pulse`}
+        >
+          Checking stock...
+        </h3>
+      ) : (
+        <h3 className={'font-bold mb-2'}>
+          {stock <= 0 && <span className="line-through">Stock unavailable</span>}
+        </h3>
+      )}
 
-    const productToCart: CartProduct = {
-        id: product.id,
-        image: product.images[0],
-        price: product.price,
-        quantity: selectedQuantity,
-        size: selectedSize!,
-        slug: product.slug,
-        title: product.title,
-    }
-
-    const addToCart = () => {
-        setAddedToCart(true);
-        if (!selectedSize) {
-            return;
-        }
-
-        addProductToCart(productToCart)
-
-        // Una vez agregado al carrito,
-        // reseteamos las elecciones
-        setAddedToCart(false);
-        setSelectedQuantity(1);
-        setSelectedSize(undefined);
-    }
-
-    return (<>
-        {isLoading ?
-            <h3 className={`${titleFont.className} p-1 bg-gray-100 text-xl rounded antialiased animate-pulse`}>Checking stock...</h3>
-            :
-            <h3 className={"font-bold mb-2"}>
-                {
-                    (stock === 0) && <span className="line-through">Stock unavailable</span>
-                }
-            </h3>
-        }
-
-        {(stock > 1) &&
-            <>
-                <SizeSelector onSizeChanged={setSelectedSize} selectedSize={selectedSize} availableSizes={product.sizes} />
-                {addedToCart && !selectedSize && <span className='fade-in text-brand-burnt-peach'>Select a size, please.</span>}
-                <QuantitySelector maxQuantity={product.inStock} quantity={selectedQuantity} onQuantityChange={setSelectedQuantity} />
-                <button onClick={addToCart} className="btn-primary my-5">add to cart</button>
-            </>
-        }
-    </>)
-}
+      {stock >= 1 && (
+        <>
+          <SizeSelector
+            onSizeChanged={setSelectedSize}
+            selectedSize={selectedSize}
+            availableSizes={product.sizes}
+          />
+          {addedToCart && !selectedSize && (
+            <span className="fade-in text-brand-burnt-peach">Select a size, please.</span>
+          )}
+          <QuantitySelector
+            maxQuantity={product.inStock}
+            quantity={selectedQuantity}
+            onQuantityChange={setSelectedQuantity}
+          />
+          <button onClick={addToCart} className="btn-primary my-5">
+            add to cart
+          </button>
+        </>
+      )}
+    </>
+  );
+};
