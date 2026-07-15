4eb8ba3b22b9c557d7f8567b9f94a763eae951d9
Javier Gimenez <jsgimenez@gmail.com>
2026-07-14 23:44:13 -0300

    chore(eslint): prettierify

--- components/product/slideshow/ProductMobileSlideshow.tsx
+++ components/product/slideshow/ProductMobileSlideshow.tsx
@@ -1,49 +1,45 @@
-'use client'
+'use client';
 
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, FreeMode, Pagination } from 'swiper/modules';
-
+import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
-
 import './slideshow.css';
-
 import Image from 'next/image';
 
 interface Props {
-    images: string[],
-    title: string,
-    className?: string
+  images: string[];
+  title: string;
+  className?: string;
 }
 
 export const ProductMobileSlideShow = ({ images, title, className }: Props) => {
-    return (
-        <div className={className}>
-            <Swiper
-                style={{ width: '100vw', height: '500px' }}
-                pagination={true}
-                navigation={true}
-                autoplay={{ delay: 2500 }}
-                modules={[FreeMode, Autoplay, Pagination]}
-                className="mySwiper2"
-            >
-                {images.map(image => (
-                    <SwiperSlide key={image}>
-                        <Image
-                            alt={title}
-                            className='object-fill'
-                            height={500}
-                            src={`/products/${image}`}
-                            width={600}
-                        />
-                    </SwiperSlide>
-
-                ))}
-            </Swiper>
-        </div>
-    );
+  return (
+    <div className={className}>
+      <Swiper
+        style={{ width: '100vw', height: '500px' }}
+        pagination={true}
+        navigation={true}
+        autoplay={{ delay: 2500 }}
+        modules={[FreeMode, Autoplay, Pagination]}
+        className="mySwiper2"
+      >
+        {images.map((image) => (
+          <SwiperSlide key={image}>
+            <Image
+              alt={title}
+              className="object-fill"
+              height={500}
+              src={`/products/${image}`}
+              width={600}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 };
