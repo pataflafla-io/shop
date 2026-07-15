30d4178b930487bbcd1f7a9bc553222a25503be9
Javier Gimenez <jsgimenez@gmail.com>
2026-07-14 22:01:49 -0300

    chore(ui): add loader component

--- app/(shop)/empty/page.tsx
+++ app/(shop)/empty/page.tsx
@@ -1,18 +1,21 @@
 import Link from "next/link";
+import { Suspense } from "react";
 import { IoCartOutline } from "react-icons/io5";
 
 export default function () {
     return (
-        <div className="flex justify-center items-center h-100 fade-in">
-            <IoCartOutline size={75} className="mx-5" />
-            <div className="flex flex-col items-start">
-                <h1 className="text-xl font-semibold">Empty cart</h1>
-                <p className="text-l mt-1" >
-                    <span>Do you want to </span>
-                    <Link className="text-brand-burnt-peach hover:underline" href="/">shop</Link>
-                    <span> somethig? </span>
-                </p>
+        <Suspense fallback="loading">
+            <div className="flex justify-center items-center h-100 fade-in">
+                <IoCartOutline size={75} className="mx-5" />
+                <div className="flex flex-col items-start">
+                    <h1 className="text-xl font-semibold">Empty cart</h1>
+                    <p className="text-l mt-1" >
+                        <span>Do you want to </span>
+                        <Link className="text-brand-burnt-peach hover:underline" href="/">shop</Link>
+                        <span> somethig? </span>
+                    </p>
+                </div>
             </div>
-        </div >
+        </Suspense>
     );
 }
