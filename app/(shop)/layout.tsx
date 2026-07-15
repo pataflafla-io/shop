b66a469e133d46317aed92164eed10fdc9415283
Javier Gimenez <jsgimenez@gmail.com>
2026-07-14 22:52:10 -0300

    styles(ui): adds Sonner > Toaster and Loader component

--- app/(shop)/layout.tsx
+++ app/(shop)/layout.tsx
@@ -1,14 +1,24 @@
-import { Footer, Sidebar, TopMenu } from "@/components/ui";
+import { Suspense } from 'react';
+import { Toaster } from 'sonner';
+import { Footer, Sidebar, TopMenu } from '@/components/ui';
+import { Loader } from '@/components/ui/loader/Loader';
 
-export default function ShopLayout({ children }: { children: React.ReactNode; }) {
-    return (
-        <main className="min-h-screen">
-            <TopMenu />
-            <Sidebar />
-            <div className="sm:px-10">
-                {children}
-            </div>
-            <Footer />
-        </main>
-    );
+export default function ShopLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <main className="min-h-screen">
+      <Suspense fallback={<Loader />}>
+        <TopMenu />
+        <Sidebar />
+        <div className="sm:px-10">{children}</div>
+        <Footer />
+        <Toaster
+          toastOptions={{
+            classNames: {
+              icon: '[&_svg]:text-white [&_svg]:fill-emerald-700',
+            },
+          }}
+        />
+      </Suspense>
+    </main>
+  );
 }
