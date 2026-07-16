b74f9f623804afa82b143cca4f7fd4419c6d2436
Javier Gimenez <jsgimenez@gmail.com>
2026-07-16 19:29:09 -0300

    styles: add same styling for both cart and checkout products in cart component

--- app/(protected)/checkout/ui/productsInCart.tsx
+++ app/(protected)/checkout/ui/productsInCart.tsx
@@ -1,33 +1,35 @@
-'use client'
+'use client';
 
-import Image from 'next/image'
-import { redirect } from 'next/navigation'
-import { useCartStore } from "@/store"
-import { currencyFormat } from '@/utils'
+import Image from 'next/image';
+import { redirect } from 'next/navigation';
+import { useCartStore } from '@/store';
+import { currencyFormat } from '@/utils';
 
 export const ProductsInCart = () => {
-
-    const productsInCart = useCartStore(state => state.cart)
-    return (
-        <>
-            {productsInCart.map(product => (
-                <div id={`${product.slug}-${product.size}`} key={`${product.slug}-${product.size}`} className="flex mb-5">
-                    <Image
-                        alt={product.title}
-                        className="mr-5 rounded"
-                        height={100}
-                        src={`/products/${product.image}`}
-                        width={100}
-                    />
-                    <div>
-                        <p className='font-bold text-lg'>{`${product.title}(size: ${product.size})`}</p>
-                        <p>{`unit price: ${currencyFormat(product.price)}`}</p>
-                        <p>{`quantity: ${product.quantity}`}</p>
-                        <p className='mt-4 font-bold'>{currencyFormat((product.price * product.quantity))}</p>
-                    </div>
-                </div>
-            ))
-            }
-        </>
-    )
-}
+  const productsInCart = useCartStore((state) => state.cart);
+  return (
+    <>
+      {productsInCart.map((product) => (
+        <div
+          id={`${product.slug}-${product.size}`}
+          key={`${product.slug}-${product.size}`}
+          className="flex mb-5"
+        >
+          <Image
+            alt={product.title}
+            className="mr-5 rounded-lg"
+            height={100}
+            src={`/products/${product.image}`}
+            width={100}
+          />
+          <div>
+            <p className="font-bold text-lg">{`${product.title}(size: ${product.size})`}</p>
+            <p>{`unit price: ${currencyFormat(product.price)}`}</p>
+            <p>{`quantity: ${product.quantity}`}</p>
+            <p className="mt-4 font-bold">{currencyFormat(product.price * product.quantity)}</p>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
