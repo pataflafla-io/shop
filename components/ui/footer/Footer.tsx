24c03adfff62b74c386a74c3be13233d28ef29e0
Javier Gimenez <jsgimenez@gmail.com>
2026-07-17 00:01:49 -0300

    chore: update url

--- components/ui/footer/Footer.tsx
+++ components/ui/footer/Footer.tsx
@@ -6,7 +6,7 @@ import { titleFont } from '@/config/fonts';
 export const Footer = () => {
   return (
     <div className="fade-in flex w-full justify-center text-md mb-10">
-      <Link href="/">
+      <Link href="https://www.pataflafla.io/" target="_blank">
         <span className={`${titleFont.className} antialiased font-bold`}>pataflafla</span>
         <span className={`${titleFont.className} antialiased`}>.io</span>
         <span>&nbsp;|&nbsp;©{new Date().getFullYear()}</span>
