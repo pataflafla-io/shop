e2d226475faa32e4b94f909df77c24aae724bd31
Javier Gimenez <jsgimenez@gmail.com>
2026-07-21 23:30:29 -0300

    chore: update import in order to not use barrel file

--- app/(shop)/gender/error.tsx
+++ app/(shop)/gender/error.tsx
@@ -1,16 +1,15 @@
-'use client'
-import { PageNotFound } from "@/components/ui";
+'use client';
+
+import { PageNotFound } from '@/components/ui/notFound/PageNotFound';
 
 interface Props {
-    error: Error
+  error: Error;
 }
 
 // Este componente se usa como 404 cuando se genera una excepción
 // El parámetro recibido tiene el mensaje de error enviado desde
 // el servidor
 export default function ({ error }: Props) {
-    const { message } = error;
-    return (
-        <PageNotFound message={message} />
-    );
+  const { message } = error;
+  return <PageNotFound message={message} />;
 }
