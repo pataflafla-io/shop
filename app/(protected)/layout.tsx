68fe486ef5d9c9ecd2d4631e22ce14340019315b
Javier Gimenez <jsgimenez@gmail.com>
2026-07-21 21:36:59 -0300

    chore: update imports in order to not use barrel file, also add Toaster component for messaging

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,6 +1,9 @@
 import { Suspense } from 'react';
-import { Footer, Navigation, Sidebar } from '@/components/ui';
+import { Toaster } from 'sonner';
+import { Footer } from '@/components/ui/footer/Footer';
 import { Loader } from '@/components/ui/loader/Loader';
+import { Navigation } from '@/components/ui/navigation/Navigation';
+import { Sidebar } from '@/components/ui/sidebar/Sidebar';
 
 export default function CheckoutLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -10,6 +13,13 @@ export default function CheckoutLayout({ children }: { children: React.ReactNode
         <Sidebar />
         <div className="sm:px-15">{children}</div>
         <Footer />
+        <Toaster
+          toastOptions={{
+            classNames: {
+              icon: '[&_svg]:text-white [&_svg]:fill-emerald-700',
+            },
+          }}
+        />
       </Suspense>
     </main>
   );
