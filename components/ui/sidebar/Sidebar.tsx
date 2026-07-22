4a1e85dec403fc0ec3746e4ba6af2dc927f75bfc
Javier Gimenez <jsgimenez@gmail.com>
2026-07-21 22:38:07 -0300

    fix: signout handling + also update deps imports to not use barrel file

--- components/ui/sidebar/Sidebar.tsx
+++ components/ui/sidebar/Sidebar.tsx
@@ -1,8 +1,11 @@
 'use client';
 
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { titleFont } from '@/config/fonts';
-import { useUIStore } from '@/store';
+import { useAddressStore } from '@/store/address/address.store';
+import { useCartStore } from '@/store/cart/cart.store';
+import { useUIStore } from '@/store/ui/sidebar.store';
 import clsx from 'clsx';
 import { signIn, signOut, useSession } from 'next-auth/react';
 import {
@@ -16,15 +19,21 @@ import {
 } from 'react-icons/io5';
 
 export const Sidebar = () => {
-  const { data: session, update } = useSession();
+  const { data: session } = useSession();
   const isAuthenticated = !!session?.user;
+
+  const router = useRouter();
 
   const isSidebarOpen = useUIStore((state) => state.isSidebarOpen);
   const closeSidebar = useUIStore((state) => state.closeSidebar);
+  const clearCart = useCartStore((state) => state.clearCart);
+  const clearAddress = useAddressStore((state) => state.clearAddress);
 
-  const handleSignout = () => {
+  const handleSignout = async () => {
+    await signOut({ callbackUrl: '/' });
+    clearCart();
+    clearAddress();
     closeSidebar();
-    signOut();
   };
 
   return (
@@ -47,9 +56,8 @@ export const Sidebar = () => {
         <div className="flex justify-between items-center">
           <p>
             <span className={`${titleFont.className} font-bold text-2xl antialiased`}>
-              pataflafla
+              demoshop
             </span>
-            <span className={`${titleFont.className} antialiased text-2xl`}>.io</span>
           </p>
 
           <IoCloseOutline
@@ -68,7 +76,7 @@ export const Sidebar = () => {
           </button>
         )}
 
-        {isAuthenticated && (
+        {session?.user.role === 'user' && (
           <>
             <Link
               href="/profile"
@@ -86,46 +94,47 @@ export const Sidebar = () => {
               <IoTicketOutline size={20} />
               <span className="ml-3 text-xl">Your orders</span>
             </Link>
-
-            <button
-              onClick={handleSignout}
-              className="flex items-center w-full p-2 rounded-lg hover:bg-black hover:text-white transition-all duration-300"
-            >
-              <IoLogOutOutline size={20} />
-              <span className="ml-3 text-xl">Sign out</span>
-            </button>
-
-            {session.user.role === 'admin' && (
-              <>
-                <div className="w-full h-px bg-gray-200 my-5" />
-                <Link
-                  href="/"
-                  onClick={() => closeSidebar()}
-                  className="flex items-center w-full mt-5 p-2 rounded-lg hover:bg-black hover:text-white transition-all duration-300"
-                >
-                  <IoShirtOutline size={20} />
-                  <span className="ml-3 text-xl">Products</span>
-                </Link>
-                <Link
-                  href="/orders"
-                  onClick={() => closeSidebar()}
-                  className="flex items-center p-2 w-full rounded-lg hover:bg-black hover:text-white transition-all duration-300"
-                >
-                  <IoTicketOutline size={20} />
-                  <span className="ml-3 text-xl">Orders</span>
-                </Link>
-                <Link
-                  href="/"
-                  onClick={() => closeSidebar()}
-                  className="flex items-center p-2 w-full rounded-lg hover:bg-black hover:text-white transition-all duration-300"
-                >
-                  <IoPeopleOutline size={20} />
-                  <span className="ml-3 text-xl">Users</span>
-                </Link>
-              </>
-            )}
           </>
         )}
+        {session?.user.role === 'admin' && (
+          <>
+            <Link
+              href="/admin/orders"
+              onClick={() => closeSidebar()}
+              className="flex items-center p-2 w-full rounded-lg hover:bg-black hover:text-white transition-all duration-300"
+            >
+              <IoTicketOutline size={20} />
+              <span className="ml-3 text-xl">Everyone's orders</span>
+            </Link>
+
+            <Link
+              href="/admin/products"
+              onClick={() => closeSidebar()}
+              className="flex items-center w-full p-2 rounded-lg hover:bg-black hover:text-white transition-all duration-300"
+            >
+              <IoShirtOutline size={20} />
+              <span className="ml-3 text-xl">Products manteinance</span>
+            </Link>
+            <Link
+              href="/admin/users"
+              onClick={() => closeSidebar()}
+              className="flex items-center p-2 w-full rounded-lg hover:bg-black hover:text-white transition-all duration-300"
+            >
+              <IoPeopleOutline size={20} />
+              <span className="ml-3 text-xl">Users management</span>
+            </Link>
+          </>
+        )}
+
+        <div className="w-full h-px bg-gray-200 my-5" />
+
+        <button
+          onClick={handleSignout}
+          className="flex items-center w-full p-2 rounded-lg hover:bg-black hover:text-white transition-all duration-300"
+        >
+          <IoLogOutOutline size={20} />
+          <span className="ml-3 text-xl">Sign out</span>
+        </button>
       </nav>
     </div>
   );
