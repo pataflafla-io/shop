47eed6b972d5372cb155c1bf25e9a251c239f734
Javier Gimenez <jsgimenez@gmail.com>
2026-07-28 12:14:26 -0300

    chore(images): add remote access for cloudinary bucket

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
