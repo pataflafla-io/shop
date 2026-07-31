e9b78beebc06e71ffec6d99cad4023f509e7d3a9
Javier Gimenez <jsgimenez@gmail.com>
2026-07-31 18:05:22 -0300

    feat(navigation): pull request #2

---
+++ components/ui/navigation/CategoriesMenu.tsx
@@ -0,0 +1,50 @@
+import Link from 'next/link';
+import { useParams } from 'next/navigation';
+import { useEffect } from 'react';
+import { useGenderSection } from '@/store/ui/genderSection.store';
+import { useUIStore } from '@/store/ui/sidebar.store';
+import clsx from 'clsx';
+
+export const CategoriesMenu = () => {
+  const { gender } = useParams();
+  const currentGender = useGenderSection((store) => store.selectedGender);
+  const setCurentGender = useGenderSection((store) => store.setCurrentGender);
+  const closeSidebar = useUIStore((store) => store.closeSidebar);
+
+  useEffect(() => {
+    closeSidebar();
+    setCurentGender(gender);
+  }, [gender]);
+
+  return (
+    <>
+      <Link
+        className={clsx(
+          'hover:bg-brand-dark-cyan m-2 rounded-md p-2 transition-all hover:text-white duration-300',
+          { 'bg-black sm:bg-brand-orange text-white': currentGender === 'men' }
+        )}
+        href="/gender/men"
+      >
+        <span className="ml-3 text-xl">Men</span>
+      </Link>
+      <Link
+        className={clsx(
+          'hover:bg-brand-dark-cyan m-2 rounded-md p-2 transition-all hover:text-white duration-300',
+          { 'bg-black sm:bg-brand-orange text-white': currentGender === 'women' }
+        )}
+        href="/gender/women"
+      >
+        <span className="ml-3 text-xl">Women</span>
+      </Link>
+      <Link
+        className={clsx(
+          'hover:bg-brand-dark-cyan m-2 rounded-md p-2 transition-all hover:text-white duration-300',
+          { 'bg-black sm:bg-brand-orange text-white': currentGender === 'kids' }
+        )}
+        href="/gender/kids"
+      >
+        <span className="ml-3 text-xl">Kids</span>
+      </Link>
+    </>
+  );
+};
--- components/ui/navigation/NavigationWithCategories.tsx
+++ components/ui/navigation/NavigationWithCategories.tsx
@@ -1,15 +1,12 @@
 'use client';
 
-import Image from 'next/image';
 import Link from 'next/link';
-import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
-import { titleFont } from '@/config/fonts';
-import { useGenderSection } from '@/store/ui/genderSection.store';
 import { useUIStore } from '@/store/ui/sidebar.store';
-import clsx from 'clsx';
 import { useSession } from 'next-auth/react';
 import { CounterCart } from '@/components/ui/counterCart/CounterCart';
+import { Logo } from '@/components/ui/logo/Logo';
+import { CategoriesMenu } from '@/components/ui/navigation/CategoriesMenu';
 
 export const NavigationWithCategories = () => {
   const [isHydrated, setIsHydrated] = useState(false);
@@ -20,69 +17,32 @@ export const NavigationWithCategories = () => {
 
   const openSidebar = useUIStore((state) => state.openSidebar);
 
-  const { gender } = useParams();
-  const currentGender = useGenderSection((store) => store.selectedGender);
-  const setCuurentGender = useGenderSection((store) => store.setCurrentGender);
-  useEffect(() => setCuurentGender(gender), [gender]);
-
   return (
-    <nav className="flex w-full items-center justify-between px-5">
-      <div>
-        <Link className="flex items-center p-5" href="/" onClick={() => setCuurentGender('')}>
-          <Image
-            alt="Logo de Pataflafla"
-            className="mx-2"
-            height={64}
-            src="/imgs/logo.png"
-            width={64}
-          />
-          <span className={`${titleFont.className} font-bold antialiased`}>demoshop</span>
-          <span className={`${titleFont.className} antialiased`}>@pataflafla.io</span>
-        </Link>
+    <nav className="flex w-full items-center justify-between sm:px-5">
+      <Logo />
+      <div className="hidden sm:block">
+        <CategoriesMenu />
       </div>
-      {
-        <div className="hidden sm:block">
-          <Link
-            className={clsx(
-              'hover:bg-brand-dark-cyan m-2 rounded-md p-2 transition-all hover:text-white duration-300',
-              { 'bg-brand-orange text-white': currentGender === 'men' }
-            )}
-            href="/gender/men"
-          >
-            men
-          </Link>
-          <Link
-            className={clsx(
-              'hover:bg-brand-dark-cyan m-2 rounded-md p-2 transition-all hover:text-white duration-300',
-              { 'bg-brand-orange text-white': currentGender === 'women' }
-            )}
-            href="/gender/women"
-          >
-            women
-          </Link>
-          <Link
-            className={clsx(
-              'hover:bg-brand-dark-cyan m-2 rounded-md p-2 transition-all hover:text-white duration-300',
-              { 'bg-brand-orange text-white': currentGender === 'kids' }
-            )}
-            href="/gender/kids"
-          >
-            kids
-          </Link>
-        </div>
-      }
+
       <div className="flex items-center">
         {isHydrated && <CounterCart />}
+        <button
+          onClick={openSidebar}
+          className="sm:hidden m-2 rounded-md p-2 hover:bg-black hover:text-white transition-all duration-300"
+        >
+          menu
+        </button>
+
         {user ? (
           <button
             onClick={openSidebar}
-            className="m-2 rounded-md p-2 hover:bg-black hover:text-white transition-all duration-300"
+            className="hidden sm:block m-2 rounded-md p-2 hover:bg-black hover:text-white transition-all duration-300"
           >
             menu
           </button>
         ) : (
           <Link
-            className="m-2 rounded-md p-2 hover:bg-black hover:text-white transition-all duration-300"
+            className="hidden sm:block m-2 rounded-md p-2 hover:bg-black hover:text-white transition-all duration-300"
             href="/auth/login"
           >
             Sign in
